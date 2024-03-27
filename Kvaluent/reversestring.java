3498eb26998e139baec186fcb39495f58deac8f0
PRAVEENKUMAR S <94734504+Prevailor@users.noreply.github.com>
2024-03-27 10:55:40 +0530

    Add files via upload

---
+++ Kvaluent/reversestring.java
@@ -0,0 +1,13 @@
+package com.assignmentk;
+
+public class reversestring {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s="AZHAR HUSSAIN";
+		for(int i=s.length()-1;i>=0;i--){
+			System.out.print(s.charAt(i));
+		}
+	}
+	
+}
